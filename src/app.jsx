c1ae7020d6ed6fb4cde4edf36e243e7c5e571b0b
wangkobong <wangkobong@gamil.com>
2021-02-12 00:45:15 +0900

    PureComponent

--- src/app.jsx
+++ src/app.jsx
@@ -13,34 +13,42 @@ class App extends Component {
   };
 
   handleIncrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
+    const habits = this.state.habits.map(item => {
+      if(item.id === habit.id){
+        return {...habit, count: habit.count + 1};
+      }
+        return item;
+    })
     this.setState({ habits });
   };
   
   handleDecrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
+    const habits = this.state.habits.map(item => {
+      if(item.id === habit.id){
+        const count = habit.count -1;
+        return {...habit, count: count < 0 ? 0 : count};
+      }
+        return item;
+    })
     this.setState({ habits });
     
   };
 
   handleDelete = habit => {
     const habits = this.state.habits.filter(item => item.id !== habit.id);
-    this.setState({habits});
+    this.setState({ habits });
   };
 
   handleAdd = name => {
     const habits = [...this.state.habits, {id: Date.now(), name, count: 0}];
-    this.setState({ habits});
+    this.setState({ habits });
   }
 
   handleReset = () => {
     const habits = this.state.habits.map(habit => {
-      habit.count = 0;
+      if(habit.count !== 0){
+        return {...habit, count: 0};
+      }
       return habit;
     });
     this.setState({ habits });
