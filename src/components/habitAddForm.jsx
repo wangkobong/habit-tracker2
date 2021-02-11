8927d6c0df64ab9f253b59dabde50d372d9c5188
wangkobong <wangkobong@gamil.com>
2021-02-11 23:56:35 +0900

    add navbar and addForm

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class HabitAddForm extends Component {
+  formRef = React.createRef();
+  inputRef = React.createRef();
+
+  onSubmit = event => {
+    event.preventDefault();
+    const name = this.inputRef.current.value;
+    name && this.props.onAdd(name);
+    //this.inputRef.current.value = '';
+    this.formRef.current.reset();
+  }
+
+  render() {
+    return (
+      <form 
+        ref={this.formRef}
+        className="add-form" 
+        onSubmit={this.onSubmit}
+      >
+        <input 
+          ref={this.inputRef}
+          type="text" 
+          className="add-input" 
+          placeholder="please enter your habit"
+        />
+        <button className="add-button">Add</button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddForm;
